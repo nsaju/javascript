fb7990ad115370e7caff170e0e9ba8c43a634c9a
Nithin Saju <88310336+nsaju@users.noreply.github.com>
2021-10-27 13:45:20 +0530

    Display all the 52 cards on the deck

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+function show(){
+    var suits=['H','C','D','S']
+    var ranks=['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+    var deck=[]
+
+for(var i=0;i<4;i++)
+    {
+    for(var j=0;j<13;j++)
+      {
+       deck.push(suits[i]+ranks[j])
+   }
+}
+document.getElementById("array").innerHTML=deck
+}
